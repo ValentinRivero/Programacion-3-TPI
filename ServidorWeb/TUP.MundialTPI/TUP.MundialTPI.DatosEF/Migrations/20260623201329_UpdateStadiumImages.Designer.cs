e8f1b254066e898062e54b96533a9b95c7069390
Estefi <estefaniaborghi5@gmail.com>
2026-06-23 17:17:38 -0300

    Update stadium background images in database seed

--- ServidorWeb/TUP.MundialTPI/TUP.MundialTPI.DatosEF/Migrations/20260623201329_UpdateStadiumImages.Designer.cs
+++ ServidorWeb/TUP.MundialTPI/TUP.MundialTPI.DatosEF/Migrations/20260623201329_UpdateStadiumImages.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TUP.MundialTPI.DatosEF;
 
@@ -10,9 +11,11 @@ using TUP.MundialTPI.DatosEF;
 namespace TUP.MundialTPI.DatosEF.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260623201329_UpdateStadiumImages")]
+    partial class UpdateStadiumImages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.9");
